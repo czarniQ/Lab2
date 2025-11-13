c02e48301be326c1d969bfeaf0cf0a4a50ad9119
czarniQ <kubaczarnik1@gmail.com>
2025-11-13 21:22:50 +0100

    Poprawka

--- zad5.py
+++ zad5.py
@@ -1,14 +1,12 @@
 # a)
-with open(r"C:\Users\czarniQ\Desktop\Laboratorium_2\notowania_gieldowe.txt", "r", encoding="utf-8") as plik: # otwiera plik
-    for linia in plik:
-        print(linia.strip()) # wypisuje dane z pliku
+with open("notowania_gieldowe.txt", "r") as plik: # otwiera plik
+    print(plik.read()) # wypisuje dane z pliku
 
 # b)
-with open(r"C:\Users\czarniQ\Desktop\Laboratorium_2\notowania_gieldowe.txt", "a", encoding="utf-8") as plik: #otwiera plik
+with open("notowania_gieldowe.txt", "a") as plik: # otwiera plik
     plik.write("\nALR, 113") # dopisuje w nowej lini dane
 
-with open(r"C:\Users\czarniQ\Desktop\Laboratorium_2\notowania_gieldowe.txt", "r", encoding="utf-8") as plik: # otwiera plik
-    for linia in plik:
-        print(linia.strip()) # wypisuje dane z pliku
+with open("notowania_gieldowe.txt", "r") as plik: # otwiera plik
+        print(plik.read()) # wypisuje dane z pliku
 
 
